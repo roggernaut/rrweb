f8da890389f9b530367bdd471062b5cb915121a5
Eoghan Murray <eoghan@getthere.ie>
2025-12-01 23:12:31 +0000

    This didn't work, tried a few things like `recordOptions<eventwithTime>['emit']` but it didn't work as the emit parameter is optional

--- packages/ws-client/src/index.ts
+++ packages/ws-client/src/index.ts
@@ -349,7 +349,7 @@ export function start(
       if (typeof configEmit === 'function') {
         configEmit(event);
       } else if (typeof window[configEmit] === 'function') {
-        (window[configEmit] as typeof recordOptions.emit)(event);
+        (window[configEmit] as any)(event);
       } else {
         console.error('Could not understand emit config option:', configEmit);
       }
