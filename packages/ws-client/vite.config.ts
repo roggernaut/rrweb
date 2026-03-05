3a3f4bee7d843d6b41f590e5650136c39888aab7
eoghanmurray <eoghanmurray@users.noreply.github.com>
2026-03-05 17:27:33 +0000

    Apply formatting changes

--- packages/ws-client/vite.config.ts
+++ packages/ws-client/vite.config.ts
@@ -8,15 +8,25 @@ const pkg = JSON.parse(
   readFileSync(path.join(__dirname, 'package.json'), 'utf-8'),
 ) as { version: string };
 
-const gitCwd = { cwd: path.resolve(__dirname, '../../'), encoding: 'utf-8' as const };
-const commitHash = spawnSync('git', ['rev-parse', 'HEAD'], gitCwd).stdout.trim();
-const isDirty = spawnSync('git', ['status', '--porcelain'], gitCwd).stdout.trim() !== '';
+const gitCwd = {
+  cwd: path.resolve(__dirname, '../../'),
+  encoding: 'utf-8' as const,
+};
+const commitHash = spawnSync(
+  'git',
+  ['rev-parse', 'HEAD'],
+  gitCwd,
+).stdout.trim();
+const isDirty =
+  spawnSync('git', ['status', '--porcelain'], gitCwd).stdout.trim() !== '';
 const commitRef = isDirty ? `${commitHash}-dirty` : commitHash;
 
 export default mergeConfig(
   baseConfig(path.resolve(__dirname, 'src/index.ts'), 'rrwebCloud'),
-  { define: {
-    __PKG_VERSION__: JSON.stringify(pkg.version),
-    __COMMIT_HASH__: JSON.stringify(commitRef),
-  } },
+  {
+    define: {
+      __PKG_VERSION__: JSON.stringify(pkg.version),
+      __COMMIT_HASH__: JSON.stringify(commitRef),
+    },
+  },
 );
