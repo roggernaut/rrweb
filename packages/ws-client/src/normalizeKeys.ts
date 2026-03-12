e9700cb948fd60e596aa6f2d16d5a54d1392cf56
Eoghan Murray <eoghan@getthere.ie>
2026-03-12 16:51:20 +0000

    Placate eslint as we are targeting older browsers:
    Error:   8:30  error  Object.entries() is not supported in Opera 29, iOS Safari 9.0-9.2, Firefox 45, Edge 13, Chrome 42  compat/compat

--- packages/ws-client/src/normalizeKeys.ts
+++ packages/ws-client/src/normalizeKeys.ts
@@ -5,7 +5,8 @@ export function normalizeKeys<T extends Record<string, unknown>>(
   const norm = (s: string) => s.toLowerCase().replace(/[-_ ]/g, '');
   const canonicalMap = new Map(Object.keys(canonical).map((k) => [norm(k), k]));
   const result = {} as Record<string, unknown>;
-  for (const [key, value] of Object.entries(input)) {
+  for (const key in input) {
+    const value = input[key];
     const match = canonicalMap.get(norm(key));
     if (match && match !== key) {
       result[match] = value;
